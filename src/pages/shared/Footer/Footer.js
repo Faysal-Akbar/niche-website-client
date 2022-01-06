7156ad8de73ba7677f5979f162a2b50c244b6dce
Faysal Akbar <faysalrj361@gmail.com>
2022-01-06 21:09:33 +0600

    Updated

--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import { Container, TextField, Typography } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
 
 const Footer = () => {
     return (
-        <Box sx={{ flexGrow: 1, backgroundColor: 'lightGray'}}>
-            <Grid container spacing={2}>
-                <Container sx={{display: 'flex', justifyContent: 'space-between', mt: 5}}>
+        <Box sx={{ bgcolor: "#2C302C"}}>
+            <Grid container spacing={2} sx={{px: 3}}>
                 <Grid item xs={12} sm={6} md={5} sx={{textAlign: 'left'}}>                   
-                    <Typography variant="h4" sx={{fontWeight: 600, mb: 3}}>
+                    <Typography variant="h4" sx={{fontWeight: 600, mb: 3, color: 'white'}}>
                         Hero <span style={{color:'#D10750'}}>Runner</span>
                     </Typography>
                     <Box sx={{borderRight: '1px solid gray'}}>
-                    <Typography variant="h6" sx={{fontSize: 14, color:'text.secondary', mb: 2, mr: 5}}>
+                    <Typography variant="h6" sx={{fontSize: 14, color: 'white', mb: 2, mr: 5}}>
                             Hero Runner looking forward to stablish there company to help the client. They can do the hard work for the clients happiness. Bicycle is one of the most favorite things for some lovely people. We like to happy there.
                     </Typography>
                     <Typography style={{color:'#D10750'}}>
@@ -25,10 +24,10 @@ const Footer = () => {
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={2} sx={{textAlign: 'left'}}>
-                    <Typography variant="h6" sx={{fontWeight: 600, textAlign: 'center'}}>
+                    <Typography variant="h6" sx={{fontWeight: 600, color: 'white', textAlign: 'center'}}>
                     PAGES
                     </Typography>
-                    <Box sx={{display: 'flex', flexDirection: 'column', ml: 2, mt: 3, color: 'text.secondary'}}>
+                    <Box sx={{display: 'flex', flexDirection: 'column', ml: 2, mt: 3, color: 'white'}}>
                         <Typography variant="button">
                         Home
                         </Typography>
@@ -44,10 +43,10 @@ const Footer = () => {
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={2} sx={{textAlign: 'left'}}>
-                    <Typography variant="h6" sx={{fontWeight: 600, textAlign: 'center'}}>
+                    <Typography variant="h6" sx={{fontWeight: 600, color: 'white', textAlign: 'center'}}>
                         MY ACCOUNT
                     </Typography>
-                    <Box sx={{display: 'flex', flexDirection: 'column', ml: 2, mt: 3, color: 'text.secondary'}}>
+                    <Box sx={{display: 'flex', flexDirection: 'column', ml: 2, mt: 3, color: 'white'}}>
                         <Typography variant="button">
                         My Account
                         </Typography>
@@ -63,15 +62,14 @@ const Footer = () => {
                     </Box>
                 </Grid>
                 <Grid item xs={12} sm={6} md={3} sx={{textAlign: 'left'}}>
-                <Typography variant="h5" sx={{fontWeight: 600, mb: 3, textAlign: 'center'}}>
+                <Typography variant="h5" sx={{fontWeight: 600, color: 'white', mb: 3, textAlign: 'center'}}>
                     NEWSLETTER
                 </Typography>
-                <Typography>
-                    Subscribe to the Hero Runner mailing list to receive updates on new arrivals, offers, and other discount information
+                <Typography sx={{color: 'white'}}>
+                    Subscribe to the Hero Runner mailing list to receive updates on new arrivals, offers, and other discount information.
                 </Typography>
-                <TextField sx={{mt: 2, width: '100%'}} id="outlined-basic" label="Write your email here" variant="outlined" />
+                <TextField sx={{mt: 2, width: '100%', backgroundColor: 'white'}} id="outlined-basic" label="Write your email here" variant="outlined" />
                 </Grid>
-                </Container>
             </Grid>
         </Box>
     );
